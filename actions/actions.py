5b0759cc0dc76d5a633d1242c2638c1105299623
ZeldaXD <miguel_molina2@hotmail.es>
2022-04-27 19:12:20 +0200

    Intentar buscar pelicula

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,23 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+import requests
+
+class ActionSearchMovie(Action):
+   def name(self) -> Text:
+      return "action_search_movie"
+
+   def run(self,
+           dispatcher: CollectingDispatcher,
+           tracker: Tracker,
+           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+      title = tracker.get_slot('title')
+      print(title)
+      current = requests.get('https://imdb-api.com/API/AdvancedSearch/k_v13u8d1y?title=' + title).json()
+      print(current)
+      dispatcher.utter_message("Hello World! from custom action")
+      return []
